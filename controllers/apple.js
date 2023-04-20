041672b9101626c867f755e66aa926237c41dceb
S562531 <122632934+S562531@users.noreply.github.com>
2023-04-19 22:25:56 -0500

    added files

--- controllers/apple.js
+++ controllers/apple.js
@@ -1,8 +1,11 @@
-var Apple = require('../models/apple');
+var apple = require('../models/apple');
 // List of all apples
+
+
+    
 exports.apple_list = async function(req, res) {
     try{
-    theapples = await Apple.find();
+    theapples = await apple.find();
     res.send(theapples);
     }
     catch(err){
@@ -11,14 +14,15 @@ exports.apple_list = async function(req, res) {
     }
    };
 // for a specific apple.
+/*
 exports.apple_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: apple detail: ' + req.params.id);
-};
+};*/
 // Handle apple create on POST.
 
 exports.apple_create_post = async function(req, res) {
     console.log(req.body)
-    let document = new Apple();
+    let document = new apple();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
@@ -47,11 +51,107 @@ exports.apple_update_put = function(req, res) {
 // Handle a show all view
 exports.apple_view_all_Page = async function(req, res) {
     try{
-    theapples = await Apple.find();
-    res.render('apple', { title: 'Apple Search Results', results: theapples });
+    theapples = await apple.find();
+    res.render('apple', { title: 'apple Search Results', results: theapples });
     }
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // for a specific apple.
+exports.apple_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await apple.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+    //Handle apple update form on PUT.
+    exports.apple_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await apple.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.apple_name)
+    toUpdate.apple_name = req.body.apple_name;
+    if(req.body.apple_price) toUpdate.apple_price = req.body.apple_price;
+    if(req.body.apple_weight) toUpdate.apple_weight = req.body.apple_weight;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+    // Handle apple delete on DELETE.
+    exports.apple_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await apple.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result) 
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+    // Handle a show one view with id specified by query
+    exports.apple_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await apple.findById( req.query.id)
+    res.render('appledetail',
+    { title: 'apple Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };  
+
+    // Handle building the view for creating a apple.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.apple_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('applecreate', { title: 'apple Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);}};
+
+    exports.apple_update_Page = async function(req, res) {
+        console.log("update view for item "+req.query.id)
+        try{
+        let result = await apple.findById(req.query.id)
+        res.render('appleupdate', { title: 'apple Update', toShow: result });
+        }
+        catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+        }
+        };
+
+        exports.apple_delete_Page = async function(req, res) {
+            console.log("Delete view for id " + req.query.id)
+            try{
+            result = await apple.findById(req.query.id)
+            res.render('appledelete', { title: 'apple Delete', toShow:
+            result });
+            }
+            catch(err){
+            res.status(500)
+            res.send(`{'error': '${err}'}`);
+            }
+            };
+       
--- routes/apple.js
+++ routes/apple.js
@@ -11,5 +11,16 @@ var express = require('express');
 const apple_controlers= require('../controllers/apple');
 var router = express.Router();
 /* GET apples */
-router.get('/', apple_controlers.apple_view_all_Page );
+// GET request for one apple.
+router.get('/', apple_controlers.apple_detail);
+
+router.get('/detail', apple_controlers.apple_view_one_Page);
+/* GET create apple page */
+router.get('/create', apple_controlers.apple_create_Page);
+
+router.get('/update', apple_controlers.apple_update_Page);
+
+router.get('/delete', apple_controlers.apple_delete_Page);
+
 module.exports = router;
+
